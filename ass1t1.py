e0e3d740bcb0e615d3b5f7a989c1d12bc4fbf8c7
pothunaman <pothunaman@gmail.com>
2025-11-14 08:13:28 +0530

    Add files via upload

---
+++ ass1t1.py
@@ -0,0 +1,13 @@
+"""Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:"""
+a=int(input("Enter first number: "))
+b=int(input("Enter second number:"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print('\n\nAddition:',add)
+print('Subtraction:',sub)
+print('Multiplication:',mul)
+print('Division:',div)
